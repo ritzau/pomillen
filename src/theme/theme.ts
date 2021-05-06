81a88786758722eea972d1b373d755c330c15172
Tobias Ritzau <tobias@ritzau.se>
2021-05-06 08:21:29 +0200

    Use installed fonts instead of web fonts
    Use system fonts on Apple and Android devices unless Lato is installed,
    in which case it has higher prio than Roboto. Fallback to web safe
    alternatives. Speed and beauty FTW!

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -18,6 +18,7 @@ export default function createPomillenTheme(prefersLightMode: boolean) {
                 'BlinkMacSystemFont',
                 'Lato',
                 'Roboto',
+                'Helvetica',
                 'sans-serif',
             ].join(','),
         }
