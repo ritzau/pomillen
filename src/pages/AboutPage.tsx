41f1685d6e6a29e409830e75185011cb1b42a80c
Tobias Ritzau <tobias@ritzau.se>
2022-06-07 15:30:08 +0200

    Tweak the tweak

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -89,7 +89,7 @@ const AboutPage: React.FC = () => {
                                     www.flaticon.com
                                 </Link>
                             
-                                Och <Link href={process.env.PUBLIC_URL + '/third-party-licenses.txt'}>här</Link>
+                                , och <Link href={process.env.PUBLIC_URL + '/third-party-licenses.txt'}>här</Link>
                                 {" "}hittar du licenserna till de andra projekten som används av Pomillen.
                             </Typography>
 
@@ -101,7 +101,7 @@ const AboutPage: React.FC = () => {
                                 Detta är version {version} av Pomillen, och du hittar koden{" "}
                                 <Link href="https://github.com/ritzau/pomillen">här</Link>, och med
                                 lite tur kan du rapportera buggar och förbättringsforslag där också.
-                                Vill du tacka så letar du upp min adress och skickar ett vykort :)
+                                Vill du skicka ett tack så leta upp min adress och skicka ett vykort :)
                             </Typography>
 
                             <Typography variant="body2" gutterBottom>
